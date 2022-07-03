b306a156d886b1663edc65c58920a4993ad431c5
FruitySyrup <106176876+FruitySyrup@users.noreply.github.com>
2022-07-02 22:56:44 -0400

    added js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const selectedRating = document.querySelectorAll('.div.rate-me-numbers div');
+
+const selectedRating = document.querySelectorAll("[data-rating]");
+
+const selectedRatingSelected = document.querySelectorAll(".selected");
+
 
 selectedRating.forEach((selectedRating) => {
-    selectedRating.addEventListener('click', () => {
-        if (selectedRating.classList.contains('selected')){
-            selectedRating.classList.remove('selected');
-        } else {
-            const selectedRatingSelected = document.querySelectorAll('.selected');
-        
-        selectedRatingSelected.forEach((selectedRatingSelected) => {
-
-            selectedRatingSelected.classList.remove('selected');
-        });
-        selectedRating.classList.add('selected');
-
-        }
-    });
-})
+	selectedRating.addEventListener("click", () => {
+		if (selectedRating.classList.contains("selected")) {
+			selectedRating.classList.remove("selected");
+		} else {
+			selectedRatingWithIsOpen.forEach((selectedRatingWithIsOpen) => {
+				selectedRatingWithIsOpen.classList.remove("selected");
+			});
+			selectedRating.classList.add(".selected");
+		}
+	});
+});
