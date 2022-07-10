b98100e9ed07af6ef26c526bc40f261e67db5413
FruitySyrup <106176876+FruitySyrup@users.noreply.github.com>
2022-07-09 21:47:16 -0400

    working on js

--- app.js
+++ app.js
@@ -1,47 +1,25 @@
+let points = document.querySelector("ul.rate-me-numbers");
+let btn = document.querySelector("button.submit");
+let selectedNumber;
+let previousSelectedNumber;
 
-// code for selecting rating
+points.addEventListener("click", checked);
+btn.addEventListener("click", submit);
 
-const selectedRating = document.querySelectorAll("[data-rating]");
+function checked(e) {
+    if (e.target.tagName === "LI") {
+        if (previousSelectedNumber) {
+            previousSelectedNumber.classList.remove("selected", "nohover");
+        }
+        e.target.classList.add("selected", "nohover");
+        selectedNumber = e.target.dataset.rating;
+        previousSelectedNumber = e.target;
+    }
+}
 
-
-selectedRating.forEach((selectedRating) => {
-	selectedRating.addEventListener("click", () => {
-		if (selectedRating.classList.contains("selected")) {
-			selectedRating.classList.remove("selected");
-		} else {
-            const selectedRatingSelected = document.querySelectorAll(".selected");
-
-			selectedRatingSelected.forEach((selectedRatingSelected) => {
-				selectedRatingSelected.classList.remove("selected");
-			});
-			selectedRating.classList.add("selected");
-		}
-	});
-});
-
-// when click submit remove hidden classes from second rate-me-page section
-
-const buttonTag = document.querySelector("a");
-const frontPage = document.querySelector("section.rate-me-page");
-const thankyouPage = document.querySelector("section.rate-me-page.hidden");
-
-buttonTag.addEventListener("click", () => {
-	frontPage.classList.toggle("hidden")
-	thankyouPage.style.display = "flex"
-});
-
-// span tag is changed to selected div/date-rating value
-
-const spanTag = document.querySelector("span");
-const ratingTag = document.querySelectorAll('#rating');
-// const dataValue = div.thankyouPage.querySelectorAll('.selected')
-// const selectedRating = document.querySelectorAll("[data-rating]")
-// let dataValue = selectedRating.getElementsbyClassName(".selected");
-
-
-	spanTag.textContent = ratingTag.dataset.rating;
-
-	// spanTag.textContent = selectedRatingSelecteda;
-
-
-console.log(ratingTag.dataset.rating);
+function submit(e) {
+    if (selectedNumber) {
+        document.getElementById(".selected-rating").innerText = selectedNumber;
+        document.querySelectorAll(".rating").forEach((e) => {e.classList.toggle("hidden")});
+    }
+}
